500e702a0ca2116efaa4e9a8567da686f10bd6c1
Erinell <ceddebois@gmail.com>
2021-12-15 17:15:00 +0100

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 export default {
   buildOptions: {
-    site: 'https://shopify-astro.netlify.app/',
+    site: 'https://tender-lamport-da2923.netlify.app/',
   },
   renderers: ['@astrojs/renderer-react'],
 };
